f595bc6bdffda82f51431f80272a7a0c6bc27ef2
AKS-RU <almetman.aks@gmail.com>
2025-09-11 22:25:08 +0300

    Окончание работы над десктоп весрией

---
+++ js/main.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // Находим все иконки с нужным классом
+  const toggles = document.querySelectorAll(".faq__item-flex-questom-toggle");
+
+  toggles.forEach((toggle) => {
+    toggle.addEventListener("click", function () {
+      // Находим следующий элемент после родителя (.faq__item-flex) — это .faq__item-flex-answer
+      const currentAnswer = this.closest(".faq__item-flex").nextElementSibling;
+      // Получаем все возможные ответы
+      const allAnswers = document.querySelectorAll(".faq__item-flex-answer");
+
+      // Сначала закрываем все
+      allAnswers.forEach((answer) => {
+        answer.style.display = "none";
+      });
+
+      // Если текущий ответ не был открыт — открываем его
+      if (currentAnswer) {
+        currentAnswer.style.display = "block";
+      }
+    });
+  });
+});
